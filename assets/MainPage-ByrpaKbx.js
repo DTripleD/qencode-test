704afe07ed4d431fcf71e8af8fcea126e4788b7f
DTripleD <DTripleD@users.noreply.github.com>
2024-03-18 12:42:09 +0000

    deploy: ac5851e12ceb65ad1cce9be561c5b9fc551996c7

--- assets/MainPage-ByrpaKbx.js
+++
@@ -1,1 +0,0 @@
-import{u as a,j as t,l as n}from"./index-DfsPqNBX.js";const i=()=>{const s=a(),o=()=>{s(n())};return t.jsxs("div",{children:["MainPage",t.jsx("button",{onClick:o,children:"Log out"})]})};export{i as default};
---
+++ assets/NotFoundPage-BrDK_WSP.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-BSoh-paa.js";const e=()=>o.jsx("div",{children:"Page not found"});export{e as default};
